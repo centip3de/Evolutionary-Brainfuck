73d76e92285a6dabddd2ccab32ae15976b955e33
centip3de <goodkingkyle@gmail.com>
2015-02-26 14:22:01 -0800

    Added support for finding inital baselines

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,44 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <chrono>
 
 #include "interp.hpp"
 #include "debug.hpp"
 
 using namespace std;
 
+Debug debug(true);
+
 void usage()
 {
     cout << "Usage: ./main <initial file>" << endl;
     exit(1);
 }
+
+unsigned int interpret_timed(Interp interp)
+{
+
+    debug.print("Beginning average time calculation");
+
+    unsigned int accumulator = 0;
+
+    for(int i = 0; i < 100000; i++)
+    {
+        chrono::high_resolution_clock::time_point start_time = chrono::high_resolution_clock::now();
+        interp.execute();
+        chrono::high_resolution_clock::time_point end_time = chrono::high_resolution_clock::now();
+
+        chrono::nanoseconds time_span = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time); 
+        accumulator += time_span.count();
+    }
+
+    debug.print("Stopping timer. Interpretation successful.");
+
+    return accumulator/100000;
+}
+
+ 
 
 int main(int argc, const char * argv[])
 {
@@ -20,14 +47,17 @@ int main(int argc, const char * argv[])
         usage();
     }
 
-    Debug debug(true);
-
     string filename = argv[1];
+    ifstream file(filename);
     string line;
     string text;
+    unsigned int base_time;
+    int base_length;
+
+    cout << "Finding basic stats for a baseline on file, " << filename << "..." << endl;
+   
     debug.print("Beginning with initial file, " + filename);
 
-    ifstream file(filename);
 
     if(file.is_open())
     {
@@ -38,11 +68,17 @@ int main(int argc, const char * argv[])
         }
         file.close();
 
-        debug.print("File contents read, beginning interpretation:"); 
-
+        debug.print("File contents read, beginning interpretation of initial file:"); 
+        
         /* Attempts to interpret the file. */
         Interp interp(text);
-        interp.execute();
+        base_time = interpret_timed(interp);
+        debug.print("Baseline time received; " + to_string(base_time) + " nanoseconds");
+
+        base_length = text.length();
+        debug.print("Baseline program length received; " + to_string(base_length));
+
+        debug.print("Beginning tree construction with baseline, " + to_string(base_time) + " as root.");
     }
     else
     {
